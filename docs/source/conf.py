a67768c461b3ce211e17f734ec31354d2f9489b0
wactbprot <thsteinbock@web.de>
2017-11-12 21:27:16 +0100

    ..

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'vpy'
 copyright = '2017, wactbprot'
-author = 'wactbprot'
+author = 'wactbprot <wactbprot@gmail.com>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -88,8 +88,10 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pyramid'
-
+#html_theme = 'classic'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='Analysis, Uncertainty, Measurands, Vacuum, Pressure',
     packages=find_packages(exclude=['docs',
@@ -42,6 +43,7 @@ setup(
                      ],
     extras_require={
         'dev': ['sphinx',
+                'sphinx_rtd_theme',
                 'autopep8',
                 ],
     },
