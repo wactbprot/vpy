723c36216713468e719e5efd43496693f7921582
wactbprot <thsteinbock@web.de>
2018-02-12 16:48:20 +0100

    +gen script

---
+++ se3_gen_base_doc.py
@@ -0,0 +1,12 @@
+import json
+from vpy.vpy_io import Io
+
+def main():
+    io = Io()
+    doc = io.get_base_doc("se3")
+
+    with open("vpy/standard/se3/base_doc.json", 'w') as f:
+            json.dump(doc, f, indent=4, ensure_ascii=False)
+
+if __name__ == "__main__":
+    main()
