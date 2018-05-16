be58e798fec7b6604d75aa01684bdaaa243b5e1c
wactbprot <thsteinbock@web.de>
2018-05-16 10:19:19 +0200

    comments

--- se2_calibration.py
+++ se2_calibration.py
@@ -6,23 +6,26 @@ from vpy.standard.se2.cal import Cal
 from vpy.standard.se2.uncert import Uncert
 
 """
-Example call:
+Bsp. Aufruf:
 python se2_calibration.py --id 'cal-2018-se2-pn-0118_0001' --log d --db 'vl_db_corr' --srv 'http://i75422:5984'
 """
 
-
 def main():
     io = Io()
-
-    base_doc = io.get_base_doc("se2")
+    # holt Messdaten aus --db
     meas_doc = io.load_doc()
-
+    # holt Konstanten ect. aus --db
+    base_doc = io.get_base_doc("se2")
+    # merge der beiden Dokumente
     doc = io.update_cal_doc(meas_doc, base_doc)
     res = Analysis(doc)
+
+    # Berechnungen-Klasse leitet vom Standard se2 ab
     cal = Cal(doc)
+    # Unsicherheits-Klasse leitet auch vom Standard se2 ab
     unc = Uncert(doc)
 
-
+    ## Bsp. Berechn. Kalibrierdruck, Unsicherh.
     cal.pressure_cal(res)
     unc.temperature_vessel(res)
 
