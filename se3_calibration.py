3ecfb423ed4b9001e8e67ec663c9f988f4ce6d4b
wactbprot <wactbprot@gmail.com>
2018-10-05 08:42:28 +0200

    ..

--- se3_calibration.py
+++ se3_calibration.py
@@ -1,14 +1,12 @@
+"""
+python se3_calibration.py --id 'cal-2018-se3-ik-4925_0001' --db 'vl_db' --srv 'http://localhost:5984'
+"""
 import sys
 import numpy as np
 from vpy.pkg_io import Io
 from vpy.analysis import Analysis
 from vpy.standard.se3.cal import Cal
 from vpy.standard.se3.uncert import Uncert
-
-"""
-Bsp. Aufruf:
-python se3_calibration.py --id 'cal-2018-se3-ik-4925_0001' --db 'vl_db' --srv 'http://localhost:5984'
-"""
 
 def main():
     io = Io()
