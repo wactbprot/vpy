6b8d87fcc1f05bfba688b3359f1b3fef3144e43c
wactbprot <wactbprot@gmail.com>
2018-10-22 14:12:05 +0200

    load + io

--- jupyter_setup.py
+++ jupyter_setup.py
@@ -6,3 +6,6 @@ vpy_path = '/home/ipynb/notebooks/vpy'
 module_path = os.path.abspath(os.path.join(vpy_path))
 if module_path not in sys.path:
     sys.path.append(module_path)
+
+from vpy.pkg_io import Io
+io = Io(path=vpy_path)
