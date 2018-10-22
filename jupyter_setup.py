4d6d0ea8ed11e688048c7624a4ce2b8dfa0aba99
wactbprot <wactbprot@gmail.com>
2018-10-22 13:37:27 +0200

    + jupyter_setup

---
+++ jupyter_setup.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import getpass
+
+vpy_path = '/home/ipynb/notebooks/vpy'
+module_path = os.path.abspath(os.path.join(vpy_path))
+if module_path not in sys.path:
+    sys.path.append(module_path)
