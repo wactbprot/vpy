22cbab1c1fda37e67070991bb61e89b0e59e03a3
wactbprot <wactbprot@gmail.com>
2018-10-23 14:03:37 +0200

    figsize 15,15

--- jupyter_setup.py
+++ jupyter_setup.py
@@ -3,6 +3,8 @@ import sys
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+plt.rcParams['figure.figsize']=15,15
+
 vpy_path = '/home/ipynb/notebooks/vpy'
 module_path = os.path.abspath(os.path.join(vpy_path))
 if module_path not in sys.path:
