23c928f1dfe314e39899c9261dd29bb1af9fc192
wactbprot <wactbprot@gmail.com>
2019-01-31 16:22:08 +0100

    u(CF) and u(e)

--- script/se3/f_s-gas_dep.py
+++ script/se3/f_s-gas_dep.py
@@ -20,27 +20,6 @@ f_n2_1 = Expansion(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0001').get("Calibrat
 p_n2_1 = Pressure(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0001').get("Calibration").get("Analysis")).get_value('cal', 'mbar')[s:e]
 plt.plot(p_n2_1, f_n2_1, label="N2 M1", marker='s', linestyle=":", markersize=10, color="blue")
 
-<<<<<<< HEAD
-f_n2_1 = Expansion(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0001').get("Calibration").get("Analysis")).get_value('f_s', '1')[-9:-1]
-p_n2_1 = Pressure(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0001').get("Calibration").get("Analysis")).get_value('cal', 'mbar')[-9:-1]
-u_n2_1 = Uncertainty(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0001').get("Calibration").get("Analysis")).get_value('total', '1')[-9:-1]
-plt.errorbar(p_n2_1, f_n2_1,  yerr=u_n2_1 *f_n2_1/2, label="N2 M1", marker='s', linestyle=":", markersize=10)
-
-f_n2_2 = Expansion(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0003').get("Calibration").get("Analysis")).get_value('f_s', '1')[-9:-1]
-p_n2_2 = Pressure(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0003').get("Calibration").get("Analysis")).get_value('cal', 'mbar')[-9:-1]
-u_n2_2 = Uncertainty(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0003').get("Calibration").get("Analysis")).get_value('total', '1')[-9:-1]
-plt.errorbar(p_n2_2, f_n2_2,  yerr=u_n2_2 *f_n2_2/2, label="N2 M2", marker='s', linestyle=":", markersize=10)
-
-f_he_1 = Expansion(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0004').get("Calibration").get("Analysis")).get_value('f_s', '1')[-9:-1]
-p_he_1 = Pressure(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0004').get("Calibration").get("Analysis")).get_value('cal', 'mbar')[-9:-1]
-u_he_1 = Uncertainty(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0004').get("Calibration").get("Analysis")).get_value('total', '1')[-9:-1]
-plt.errorbar(p_he_1, f_he_1,  yerr=u_he_1 *f_he_1/2, label="He M1", marker='8', linestyle=":", markersize=10)
-
-f_he_2 = Expansion(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0005').get("Calibration").get("Analysis")).get_value('f_s', '1')[-9:-1]
-p_he_2 = Pressure(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0005').get("Calibration").get("Analysis")).get_value('cal', 'mbar')[-9:-1]
-u_he_2 = Uncertainty(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0005').get("Calibration").get("Analysis")).get_value('total', '1')[-9:-1]
-plt.errorbar(p_he_2, f_he_2,  yerr=u_he_2 *f_he_2/2, label="He M2", marker='8', linestyle=":", markersize=10)
-=======
 f_n2_2 = Expansion(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0003').get("Calibration").get("Analysis")).get_value('f_s', '1')[s:e]
 p_n2_2 = Pressure(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0003').get("Calibration").get("Analysis")).get_value('cal', 'mbar')[s:e]
 plt.plot(p_n2_1, f_n2_2, label="N2 M2", marker='8', linestyle=":", markersize=10, color="blue")
@@ -60,7 +39,6 @@ plt.plot(p_ar_1, f_ar_1, label="Ar M1", marker='s', linestyle=":", markersize=10
 f_ar_2 = Expansion(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0007').get("Calibration").get("Analysis")).get_value('f_s', '1')[s:e]
 p_ar_2 = Pressure(io.get_doc_db('cal-2019-se3|frs5-vg-1001_0007').get("Calibration").get("Analysis")).get_value('cal', 'mbar')[s:e]
 plt.plot(p_ar_2, f_ar_2, label="Ar M2", marker='8', linestyle=":", markersize=10, color="orange")
->>>>>>> 1de96852b1a72e15e1a824cee3148819dae2b592
 
 plt.legend()
 plt.title('SE3 expansion $f_s$ (Nitrogen, Helium, Argon)')
