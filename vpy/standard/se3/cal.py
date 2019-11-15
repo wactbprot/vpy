e2d1ae912f52ff6057f2f0119bcc9a8198495869
Thomas Bock <thomas.bock@ptb.de>
2019-11-15 10:10:38 +0100

    dh vi

--- vpy/standard/se3/cal.py
+++ vpy/standard/se3/cal.py
@@ -519,6 +519,7 @@ class Cal(Se3):
         p = p * self.Cons.get_conv(from_unit=p_unit, to_unit="Pa")
 
         M = self.Cons.get_value("molWeight_{gas}".format(gas=gas), "kg/mol")
+        print(M)
         R = self.Cons.get_value("R","Pa m^3/mol/K" )
         T = self.Cons.get_value("referenceTemperature", "K")
         g = self.Cons.get_value("g", "m/s^2")
