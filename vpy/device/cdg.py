4c7cb36c88bff863768aecd1ecc8e20ba1853127
Thomas Bock <thomas.bock@ptb.de>
2019-11-21 16:20:42 +0100

    interpol wo uncertainty iv

--- vpy/device/cdg.py
+++ vpy/device/cdg.py
@@ -252,8 +252,9 @@ class Cdg(Device):
 
         extr_p_high = np.array([self.max_p*(1.0 + self.range_extend)])
         d = extr_p_high - p
+        print(d)
         j = self.get_dmin_idx(d[0])
-       
+        print(j)
         extr_e_high = np.array([e[j]])
 
         ret_p = np.concatenate( (extr_p_low, p, extr_p_high), axis=None)
