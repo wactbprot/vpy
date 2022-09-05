7daec90cdabbdf432ceec5beb914c171a06adbf9
Thomas Bock <thomas.bock@ptb.de>
2022-09-05 11:58:15 +0200

    FRS5 Uncert 30Pa < p < 133Pa

--- vpy/standard/se3/uncert.py
+++ vpy/standard/se3/uncert.py
@@ -487,7 +487,7 @@ class Uncert(Se3):
     def cmc(self, ana):
         p_list = ana.pick("Pressure", "cal", self.pressure_unit)
 
-        u = np.asarray([np.piecewise(p, [p <= 0.027, (p > 0.027 and p <= 0.3), (p > 0.3 and p <= 0.73), (p >0.73 and p <= 9.), (p > 9. and p <= 1000.), (p > 1000. and p <= 8000.),  8000. < p]
-                                       ,[0.0014,                        0.001,                 0.00092,              0.00086,                 0.00075,                   0.00019,  0.00014] ).tolist() for p in p_list])
+        u = np.asarray([np.piecewise(p, [p <= 0.027, (p > 0.027 and p <= 0.3), (p > 0.3 and p <= 0.73), (p > 0.73 and p <= 9.), (p > 9. and p <= 30.), (p > 30. and p <= 133.),            (p > 133. and p <= 1000.), (p > 1000. and p <= 8000.), 8000. < p],
+                                        [0.0014,     0.001,                   0.00092,                0.00086,              0.00075,              ((0.0245/p)**2 + 2.24e-5**2)**0.5, 0.00075,                  0.00019,                  0.00014] ).tolist() for p in p_list])
 
         ana.store("Uncertainty", "standard", u , "1")
